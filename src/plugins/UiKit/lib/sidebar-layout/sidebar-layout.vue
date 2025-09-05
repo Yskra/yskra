e201271a7cbae14b5840b0203f68e96f8c35d1d6
Xinos <40886699+userXinos@users.noreply.github.com>
2025-09-05 23:34:45 +0700

    uikit: hide sidebar logic

--- src/plugins/UiKit/lib/sidebar-layout/sidebar-layout.vue
+++ src/plugins/UiKit/lib/sidebar-layout/sidebar-layout.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
 import { useActiveElement } from '@vueuse/core';
-import { computed, ref, useTemplateRef } from 'vue';
+import { computed, onUnmounted, ref, useTemplateRef } from 'vue';
 
 const sidebarContainerRef = useTemplateRef<HTMLElement>('sidebarContainerRef');
 const activeElement = useActiveElement();
 
 const hoveredContainer = ref(false);
 const compactSidebar = computed(() => hoveredContainer.value ? false : (activeElement.value && sidebarContainerRef.value) ? !sidebarContainerRef.value?.contains(activeElement.value) : false);
+let hoverTimeout: NodeJS.Timeout | undefined;
+
+onUnmounted(() => {
+  clearTimeout(hoverTimeout);
+});
+
+function onMouseEnter() {
+  if (hoverTimeout) {
+    clearTimeout(hoverTimeout);
+    hoverTimeout = undefined;
+  }
+  hoveredContainer.value = true;
+}
+
+function onMouseLeave() {
+  hoverTimeout = setTimeout(() => {
+    hoveredContainer.value = false;
+  }, 500);
+}
 </script>
 
 <template>
   <div class="flex">
     <div
       ref="sidebarContainerRef"
-      @mouseenter="hoveredContainer = true"
-      @mouseleave="hoveredContainer = false"
+      @mouseenter="onMouseEnter"
+      @mouseleave="onMouseLeave"
     >
       <slot name="sidebar" :compact-sidebar="compactSidebar" />
     </div>
