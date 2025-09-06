2cc29c4a76c2b5cea99a0984021e7a380453470a
Xinos <40886699+userXinos@users.noreply.github.com>
2025-09-06 16:11:05 +0700

    uikit: remove uncompact sidebar on hover

--- src/plugins/UiKit/lib/sidebar-layout/sidebar-layout.vue
+++ src/plugins/UiKit/lib/sidebar-layout/sidebar-layout.vue
@@ -1,40 +1,15 @@
 <script setup lang="ts">
 import { useActiveElement } from '@vueuse/core';
-import { computed, onUnmounted, ref, useTemplateRef } from 'vue';
+import { computed, useTemplateRef } from 'vue';
 
 const sidebarContainerRef = useTemplateRef<HTMLElement>('sidebarContainerRef');
 const activeElement = useActiveElement();
-
-const hoveredContainer = ref(false);
-const compactSidebar = computed(() => hoveredContainer.value ? false : (activeElement.value && sidebarContainerRef.value) ? !sidebarContainerRef.value?.contains(activeElement.value) : false);
-let hoverTimeout: NodeJS.Timeout | undefined;
-
-onUnmounted(() => {
-  clearTimeout(hoverTimeout);
-});
-
-function onMouseEnter() {
-  if (hoverTimeout) {
-    clearTimeout(hoverTimeout);
-    hoverTimeout = undefined;
-  }
-  hoveredContainer.value = true;
-}
-
-function onMouseLeave() {
-  hoverTimeout = setTimeout(() => {
-    hoveredContainer.value = false;
-  }, 500);
-}
+const compactSidebar = computed(() => (activeElement.value && sidebarContainerRef.value) ? !sidebarContainerRef.value?.contains(activeElement.value) : false);
 </script>
 
 <template>
   <div class="flex">
-    <div
-      ref="sidebarContainerRef"
-      @mouseenter="onMouseEnter"
-      @mouseleave="onMouseLeave"
-    >
+    <div ref="sidebarContainerRef">
       <slot name="sidebar" :compact-sidebar="compactSidebar" />
     </div>
     <div
